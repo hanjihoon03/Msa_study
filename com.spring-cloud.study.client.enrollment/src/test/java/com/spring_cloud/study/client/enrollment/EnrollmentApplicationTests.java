93cbcd2d8fe9396229d7fd6263ec1539e4790d38
hanjihoon03 <gkswlgns7653@gmail.com>
2025-02-07 10:10:33 +0900

    first commit

---
+++ com.spring-cloud.study.client.enrollment/src/test/java/com/spring_cloud/study/client/enrollment/EnrollmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_cloud.study.client.enrollment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ com.spring-cloud.study.client.lecture/src/main/java/com/spring_cloud/study/client/lecture/LectureApplication.java
@@ -0,0 +1,13 @@
+package com.spring_cloud.study.client.lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureApplication.class, args);
+	}
+
+}

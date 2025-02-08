7b78fdcee7ba9d028b777133110249363365fa2f
hanjihoon03 <gkswlgns7653@gmail.com>
2025-02-08 22:47:49 +0900

    진행중

---
+++ com.spring-cloud.study.client.auth/src/main/java/com/spring_cloud/study/client/auth/model/User.java
@@ -0,0 +1,24 @@
+package com.spring_cloud.study.client.auth.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String username;
+    private String password;
+    private UserRoleEnum userRoleEnum;
+
+}

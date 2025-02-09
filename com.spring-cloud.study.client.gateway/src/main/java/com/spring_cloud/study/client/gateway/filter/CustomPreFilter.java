2f224c6eb1c326d400bb4b422c602c2d1e2ebcdf
hanjihoon03 <gkswlgns7653@gmail.com>
2025-02-09 15:49:49 +0900

    gateway fillter 구현

---
+++ com.spring-cloud.study.client.gateway/src/main/java/com/spring_cloud/study/client/gateway/filter/CustomPreFilter.java
@@ -0,0 +1,29 @@
+package com.spring_cloud.study.client.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class CustomPreFilter implements GlobalFilter, Ordered {
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        log.info("PreFilter Request URI : {}", request.getURI());
+
+        return chain.filter(exchange);
+
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.HIGHEST_PRECEDENCE;
+    }
+}
---
+++ com.spring-cloud.study.client.gateway/src/main/java/com/spring_cloud/study/client/gateway/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,75 @@
+package com.spring_cloud.study.client.gateway.filter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import javax.crypto.SecretKey;
+
+@Slf4j
+@Component
+public class JwtAuthenticationFilter implements GlobalFilter {
+
+    @Value("${service.jwt.secret-key}")
+    private String secretKey;
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String path = exchange.getRequest().getURI().getPath();
+
+        if (path.equals("/auth/signIn") || path.equals("/auth/signUp")) {
+            return chain.filter(exchange);
+        }
+
+        String token = extractToken(exchange);
+
+        if (token == null || !validateToken(token, exchange)) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        return chain.filter(exchange);
+
+
+    }
+
+    private String extractToken(ServerWebExchange exchange) {
+        String authorizationHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            return authorizationHeader.substring(7);
+        }
+        return null;
+    }
+
+    private Boolean validateToken(String token, ServerWebExchange exchange) {
+        try {
+            SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64URL.decode(secretKey));
+
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .verifyWith(key)
+                    .build().parseSignedClaims(token);
+
+            log.info("Jwt payload :" + claimsJws.getPayload().toString());
+
+            Claims claimsJwsBody = claimsJws.getBody();
+            exchange.getRequest().mutate()
+                    .header("X-User-Id", claimsJwsBody.get("user_id").toString())
+                    .header("X-Role", claimsJwsBody.get("role").toString())
+                    .build();
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
